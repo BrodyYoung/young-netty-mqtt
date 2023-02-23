acc319b369e6ddff0b8e2b49103b6b95a76a862a
aidenyoung <yyb1415>
2023-02-23 18:17:54 +0800

    修改项目结构

--- mqtt-broker/src/main/java/yyb/server/MqttNettyServer.java
+++ mqtt-broker/src/main/java/yyb/server/MqttNettyServer.java
@@ -1,13 +1,13 @@
-package com.yyb.server;
+package yyb.server;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.ChannelOption;
-import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.ChannelPipeline;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.mqtt.MqttDecoder;
 import io.netty.handler.codec.mqtt.MqttEncoder;
 
